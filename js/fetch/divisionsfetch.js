573844aa12e3349f1cf59ae0a14568789009d194
wporta06 <portawalid@gmail.com>
2021-10-15 00:17:58 +0100

    first

--- js/fetch/divisionsfetch.js
+++ js/fetch/divisionsfetch.js
@@ -1,40 +1,82 @@
 const divisionstbody = document.querySelector('.divisionstbody');
+const addpostform = document.querySelector('.add-post-form');
+const divisioninp = document.getElementById('divisioninp');
+
 let output = '';
+let url = 'http://localhost/stgapi/division'
 
-
-
+// read
 const renderPosts = (posts) => {
     console.log(posts);
     posts.forEach(datar => {
         output +=
 
             `<tr data-id="${datar.id}">
-            <td class="odd" contenteditable="true"  >${datar.title}</td>
-            <td class="odd">
-                <a class="edit fas fa-trash-alt fa-1x ml-3" id="delete-row">d</a>
-                <a class="edit" id="edit-row" data-toggle="modal"><i  class="fas fas fa-save fa-1x ml-3"></i></a>
-            </td>
+        <td class="odd" contenteditable="true"  >${datar.designiation}</td>
+        <td class="odd">
+        <a class="" id="delete-row">delete</a>
+        <a class="edit" id="edit-row" data-toggle="modal"><i  class="fas fas fa-save fa-1x ml-3"></i></a>
+        </td>
         </tr>`
 
     });
     divisionstbody.innerHTML = output;
 }
 
-fetch("http://localhost/apitraversy/api/post/read.php")
+fetch(url)
     .then(res => res.json())
-    .then(data => renderPosts(data));
+    .then(data => renderPosts(data))
 
 
+// create new divetion
+addpostform.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log("clicked");
+
+    fetch(url, {
+            method: 'POST',
+            header: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                designiation: divisioninp.value,
+            })
+        })
+        .then(res => res.json())
+        .then(data => {
+            const dataArr = [];
+            dataArr.push(data);
+            renderPosts(dataArr);
+        })
+        .then(() => location.reload());
+})
+
+// delete divesion
 
 divisionstbody.addEventListener('click', (e) => {
-    console.log(e.target.id);
+    // console.log(e.target.id);
     e.preventDefault();
     let delBtnIsPressed = e.target.id == "delete-row";
     let editBtnIsPressed = e.target.id == "edit-row";
 
-    console.log(e.target.parentElement.dataset.id);
-    if (delBtnIsPressed) {
-
-    }
     let id = e.target.parentElement.parentElement.dataset.id;
+    if (delBtnIsPressed) {
+        console.log("eeee");
+        const myDataObject = { id: id }
+        fetch(url, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(myDataObject)
+            })
+            .then(response => {
+                return response.json()
+            })
+            .then(data =>
+                // this is the data we get after doing the delete request, do whatever you want with this data
+                console.log(data)
+            );
+    }
+
 })
--- js/fetch/marquesfetch.js
+++ js/fetch/marquesfetch.js
@@ -1,7 +1,8 @@
+console.log("zadza");
 const marquestbody = document.querySelector('.marquestbody');
 let output = '';
 
-fetch("http://localhost/apitraversy/api/post/read.php")
+fetch("http://localhost/stgapi/marque")
     .then(res => res.json())
     .then(data => {
         console.log(data);
@@ -10,7 +11,7 @@ fetch("http://localhost/apitraversy/api/post/read.php")
 
                 ` <tr>
 
-                <td class="odd" contenteditable="true">${datar.author}</td>
+                <td class="odd" contenteditable="true">${datar.marque}</td>
                 <td class="odd">
                     <a class="edit" title="delete" data-toggle="modal"><i id="delbtn" class="fas fa-trash-alt fa-1x ml-3"></i></a>
                     <a class="edit" title="edit" data-toggle="modal"><i  class="fas fas fa-save fa-1x ml-3"></i></a>
